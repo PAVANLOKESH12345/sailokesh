70b750b8de57e305a7b17fe93b72cf79dfdc9fc0
PAVANLOKESH12345 <venkatasailokesh.alahari@gmail.com>
2024-03-15 11:22:28 +0530

    sending local

--- src/Practice.java
+++ src/Practice.java
@@ -1,9 +1,10 @@
 
-public class Practice {
+public class Practice 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+           System.out.println("   done");
 	}
 
 }
