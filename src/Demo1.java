6a1c83c98af9d67610787214576b961864cbbf89
PAVANLOKESH12345 <161283146+PAVANLOKESH12345@users.noreply.github.com>
2024-03-15 11:43:57 +0530

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,5 +1,7 @@
-public class Ecllipsedemo {   
-	public static void main(String[] args) {
+public class Ecllipsedemo 
+{   
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("sailokesh");
 	}
